4b40ca65b1a7c6b6cfad1c69a4777e2b9199ba2e
Mohana1022 <pmohana1022@gmail.com>
2025-11-18 17:55:58 +0530

    dfdhg

---
+++ src/main/java/calci/Calci.java
@@ -0,0 +1,11 @@
+package calci;
+
+
+public interface Calci {
+	
+	public void add(int a, int b);
+	public void sub(int a, int b);
+	public void mul(int a, int b);
+	public void div(int a, int b);
+	
+}
